346a295791ae12f6946801cecd3d1436b836d998
Farhad <farhadh121@gmail.com>
2017-04-11 21:39:50 -0400

    maven changes

--- out/artifacts/server_war_exploded/front/js/common.js
+++
@@ -1,6 +0,0 @@
-var domain =  "http://52.88.252.192:8080";
-
-function getDomain(){
-
-    return domain;
-}
--- radio/src/main/java/com/radio/svc/dalc/IArtistFeatureDA.java
+++
@@ -1,9 +0,0 @@
-package com.radio.svc.dalc;
-
-import com.radio.entity.hibernate.ArtistFeatureEntity;
-
-/**
- * Created by farhad on 8/30/14.
- */
-public interface IArtistFeatureDA extends IGenericDA<ArtistFeatureEntity, Long> {
-}
